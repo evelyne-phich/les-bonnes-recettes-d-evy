35a81f598c0b75f29a1e6256ade091b48ebc9523
Evelyne PHICH <evelyne.phich@gmail.com>
2021-02-12 22:33:05 +0100

    sideNav from bottom

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+function openSideNavStarters() {
+  let sideNavStarters = document.getElementById("sideNavStarters");
+  console.log(sideNavStarters);
+  sideNavStarters.classList.add("isOpen");
+}
+
+function openSideNavDishes() {
+  let sideNavDishes = document.getElementById("sideNavDishes");
+  sideNavDishes.classList.add("isOpen");
+}
