b0057b64a3b4557b9ffcf646848cb7c7cbe9b7de
Evelyne PHICH <evelyne.phich@gmail.com>
2021-02-13 14:07:34 +0100

    close sideNav

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,13 @@
 function openSideNav() {
   let sideNav = document.getElementById("sideNav");
-  sideNav.classList.add("isOpen");
+  sideNav.classList.add("sideNavIsOpen");
+  let outsideOfSideNav = document.getElementById("outsideOfSideNav");
+  outsideOfSideNav.classList.add("outsideOfSideNavIsOpen");
+}
+
+function closeSideNav() {
+  let sideNav = document.getElementById("sideNav");
+  sideNav.classList.remove("sideNavIsOpen");
+  let outsideOfSideNav = document.getElementById("outsideOfSideNav");
+  outsideOfSideNav.classList.remove("outsideOfSideNavIsOpen");
 }
