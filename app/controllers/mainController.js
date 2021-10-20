b797b854ca88e5c7aa21975fccc4188e4b5e8e78
evelyne-phich <evelyne.phich@gmail.com>
2021-10-20 20:53:30 +0200

    testing datamapper

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -2,32 +2,28 @@ const dataMapper = require("../dataMapper");
 
 const mainController = {
   getHomePage: (req, res) => {
-    // dataMapper.getAllRecipes((err, result) => {
-    //   if (err) {
-    //     return console.error(err);
-    //   }
+    dataMapper.getAllRecipes((err, result) => {
+      if (err) {
+        return console.error(err);
+      }
 
-    //   const recipes = result.rows;
-    //   const countries = [];
+      const recipes = result.rows;
+      const countries = [];
 
-    //   for (const recipe of recipes) {
-    //     if (
-    //       !countries.find(
-    //         (country) => recipe.country.toLowerCase() === country.toLowerCase()
-    //       )
-    //     ) {
-    //       countries.push(recipe.country);
-    //     }
-    //   }
+      for (const recipe of recipes) {
+        if (
+          !countries.find(
+            (country) => recipe.country.toLowerCase() === country.toLowerCase()
+          )
+        ) {
+          countries.push(recipe.country);
+        }
+      }
 
-    //   res.render("homePage", {
-    //     recipes,
-    //     countries,
-    //   });
-    // });
-    res.render("homePage", {
-      recipes: [],
-      countries: [],
+      res.render("homePage", {
+        recipes,
+        countries,
+      });
     });
   },
 };
